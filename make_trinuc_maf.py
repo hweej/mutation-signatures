e86842e016d17239a8ff678c588caf7306cddb7c
Jason Hwee <hweej@mskcc.org>
2019-04-11 11:58:59 -0400

    Retab entire file. Add support for future print_function to not only support python3.

--- make_trinuc_maf.py
+++ make_trinuc_maf.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import subprocess
 import sys
 from string import strip, join
 
 if len(sys.argv) < 3:
-	print("Usage: python make_trinuc_maf.py [source maf path] [target maf path]")
-	sys.exit(0)
+        print("Usage: python make_trinuc_maf.py [source maf path] [target maf path]")
+        sys.exit(0)
 
 from_maf = sys.argv[1]
 to_maf = sys.argv[2]
@@ -19,19 +20,19 @@ tmpf = open("___tmp.maf",'w')
 print("Ignoring non-SNP mutations")
 firstline = fromf.readline()
 while firstline.startswith("#"):
-	firstline = fromf.readline()
+        firstline = fromf.readline()
 
 lines = list(map(strip, firstline.split('\t')))
 indels = []
 type_col = lines[0].index("Variant_Type")
-	
+        
 for line in fromf:
-	split_line = list(map(strip, line.split('\t')))
-	type = split_line[type_col]
-	if type == "SNP":
-		lines.append(split_line)
-	if type != "SNP": 
-		indels.append(split_line)
+        split_line = list(map(strip, line.split('\t')))
+        type = split_line[type_col]
+        if type == "SNP":
+                lines.append(split_line)
+        if type != "SNP": 
+                indels.append(split_line)
 
 tmpf.write(join([join(x, '\t') for x in lines], '\n'))
 tmpf.flush()
@@ -57,27 +58,27 @@ trinucf = open("___tmp.tsv","r")
 i = 0
 
 def normalizeTrinuc(trinuc):
-	comp = {'A':'T',\
-		'T':'A',\
-		'C':'G',\
-		'G':'C'}
-	if not trinuc[1] in ['C','T']:
-		return join([comp[x] for x in trinuc][::-1], '')
-	else:
-		return trinuc
+        comp = {'A':'T',\
+                'T':'A',\
+                'C':'G',\
+                'G':'C'}
+        if not trinuc[1] in ['C','T']:
+                return join([comp[x] for x in trinuc][::-1], '')
+        else:
+                return trinuc
 
 for line in trinucf:
-	i += 1
-	trinuc = line.split('\t')[1].strip()
-	try:
-		trinuc = normalizeTrinuc(trinuc)
-	except:
-		pass
-	lines[i].append(trinuc)
+        i += 1
+        trinuc = line.split('\t')[1].strip()
+        try:
+                trinuc = normalizeTrinuc(trinuc)
+        except:
+                pass
+        lines[i].append(trinuc)
 
 # Add back indels
 for line in indels:
-	line.append('')
+        line.append('')
 
 lines.extend(indels)
 
